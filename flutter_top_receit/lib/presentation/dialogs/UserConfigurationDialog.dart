aeb8e6c753902cb08af6b83a10957e8ccf244fc3
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2025-03-02 18:53:13 +0100

    Implementacion de guardado de avatar en firebase

--- flutter_top_receit/lib/presentation/dialogs/UserConfigurationDialog.dart
+++ flutter_top_receit/lib/presentation/dialogs/UserConfigurationDialog.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_top_receit/data/models/user_model.dart';
@@ -5,6 +10,7 @@ import 'package:flutter_top_receit/domain/entities/user_entity.dart';
 import 'package:flutter_top_receit/presentation/blocs/auth/auth_bloc.dart';
 import 'package:flutter_top_receit/presentation/blocs/auth/auth_event.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserConfigurationDialog extends StatefulWidget {
   final UserEntity user;
@@ -29,13 +35,59 @@ class _UserConfigurationDialogState extends State<UserConfigurationDialog> {
     'Salado',
   ];
 
+  dynamic _avatarFile;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   void initState() {
     super.initState();
     _usernameController.text = widget.user.username;
-    _selectedPreferences = List.from(
-        widget.user.preferences); // Initialize with user's preferences
+    _selectedPreferences = List.from(widget.user.preferences);
     _avatarController.text = widget.user.avatar;
+  }
+
+  Future<void> _pickAvatar() async {
+    if (kIsWeb) {
+      FilePickerResult? result =
+          await FilePicker.platform.pickFiles(type: FileType.image);
+      if (result != null) {
+        setState(() {
+          _avatarFile = result.files.single.bytes;
+        });
+      }
+    } else {
+      final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        setState(() {
+          _avatarFile = File(pickedFile.path);
+        });
+      }
+    }
+  }
+
+  Future<String?> _uploadAvatar(String userId) async {
+    if (_avatarFile == null) return null;
+
+    try {
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('avatars/${DateTime.now().toString()}');
+
+      final metadata = SettableMetadata(contentType: 'image/jpeg');
+
+      if (kIsWeb) {
+        await storageRef.putData(_avatarFile, metadata);
+      } else {
+        await storageRef.putFile(_avatarFile, metadata);
+      }
+
+      final downloadUrl = await storageRef.getDownloadURL();
+      print('Download URL: $downloadUrl');
+      return downloadUrl;
+    } catch (e) {
+      print('Error uploading avatar: $e');
+      return null;
+    }
   }
 
   @override
@@ -86,6 +138,14 @@ class _UserConfigurationDialogState extends State<UserConfigurationDialog> {
                 });
               },
             ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _pickAvatar,
+              child: const Text('Seleccionar nuevo avatar'),
+            ),
+            if (_avatarFile != null) ...[
+              const Text('Avatar seleccionado'),
+            ],
           ],
         ),
       ),
@@ -97,11 +157,24 @@ class _UserConfigurationDialogState extends State<UserConfigurationDialog> {
           child: Text(AppLocalizations.of(context)!.cancel_button),
         ),
         TextButton(
-          onPressed: () {
+          onPressed: () async {
+            String? avatarUrl = _avatarController.text;
+            if (_avatarFile != null) {
+              final uploadedUrl = await _uploadAvatar(widget.user.id);
+              if (uploadedUrl != null) {
+                avatarUrl = uploadedUrl;
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Error uploading avatar')),
+                );
+                return;
+              }
+            }
+
             final updatedUser = widget.user.copyWith(
               username: _usernameController.text,
               preferences: _selectedPreferences,
-              avatar: _avatarController.text,
+              avatar: avatarUrl,
             );
 
             final updatedUserModel = UserModel(
@@ -112,11 +185,14 @@ class _UserConfigurationDialogState extends State<UserConfigurationDialog> {
               preferences: updatedUser.preferences,
             );
 
+            // ignore: use_build_context_synchronously
             context
                 .read<AuthBloc>()
                 .add(UpdateUserEvent(user: updatedUserModel));
 
+            // ignore: use_build_context_synchronously
             Navigator.pop(context);
+            // ignore: use_build_context_synchronously
             Navigator.pop(context);
           },
           child: Text(AppLocalizations.of(context)!.accept_button),
